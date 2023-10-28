dca2c4c63411635ad420ffdbccf9c1fe84793c8e
rossclay <clay.rossclay@gmail.com>
2023-10-28 11:44:21 -0400

    fixed randomizer functionality

--- script.js
+++ script.js
@@ -15,6 +15,16 @@ function setGradient() {
 	css.textContent = body.style.background + ";";
 }
 
+function setRandomGradient() {
+	body.style.background =
+		"linear-gradient(to right, " +
+		generateRandomColors() +
+		", " +
+		generateRandomColors();
+
+	css.textContent = body.style.background + ";";
+}
+
 function generateRandomColors() {
 	var r = Math.floor(Math.random() * 256);
 	var g = Math.floor(Math.random() * 256);
